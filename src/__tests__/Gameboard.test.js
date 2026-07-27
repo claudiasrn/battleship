10bba4908686d7e47c39c9a5ce6030bcb1c8ba65
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-27 04:37:35 +0200

    test(Gameboard): add tests

---
+++ src/__tests__/Gameboard.test.js
@@ -0,0 +1,142 @@
+import { Gameboard } from "../Gameboard.js";
+
+test("ship place occupies right coordinates", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	gameboard.placeShip(coordinates);
+
+	expect(gameboard.isOver()).toBe(false);
+
+	gameboard.receiveAttack([4, 3]);
+	gameboard.receiveAttack([4, 4]);
+	gameboard.receiveAttack([4, 5]);
+
+	expect(gameboard.isOver()).toBe(true);
+});
+
+test("placing an overlapping ship doesn't corrupt the first ship's cells", () => {
+	const gameboard = new Gameboard();
+	const coordinates1 = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	const coordinates2 = [
+		[4, 4],
+		[4, 5],
+		[4, 6],
+	];
+
+	gameboard.placeShip(coordinates1);
+	gameboard.placeShip(coordinates2);
+
+	expect(gameboard.receiveAttack([4, 3])).toBe("x");
+	expect(gameboard.receiveAttack([4, 4])).toBe("x");
+	expect(gameboard.receiveAttack([4, 5])).toBe("x");
+});
+
+test("attacking an empty cell, will mark it as a miss", () => {
+	const gameboard = new Gameboard();
+	expect(gameboard.receiveAttack([4, 3])).toBe("w");
+});
+
+test("attacking a cell with a ship, will mark it as a hit", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	gameboard.placeShip(coordinates);
+
+	expect(gameboard.receiveAttack([4, 3])).toBe("x");
+});
+
+test("attacking a cell that has been attacked already wont work", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	gameboard.placeShip(coordinates);
+	gameboard.receiveAttack([4, 3]);
+	gameboard.receiveAttack([0, 0]);
+
+	expect(gameboard.receiveAttack([4, 3])).toBe(undefined);
+	expect(gameboard.receiveAttack([0, 0])).toBe(undefined);
+});
+
+test("if no ships have been placed it isn't over or not over", () => {
+	const gameboard = new Gameboard();
+
+	expect(gameboard.isOver()).toBe(undefined);
+});
+
+test("if ships are placed and not sunk the game shouldn't be over", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	gameboard.placeShip(coordinates);
+	gameboard.receiveAttack([4, 3]);
+
+	expect(gameboard.isOver()).toBe(false);
+});
+
+test("if ships are placed and all sunk the game should be over", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	gameboard.placeShip(coordinates);
+	gameboard.receiveAttack([4, 3]);
+	gameboard.receiveAttack([4, 4]);
+	gameboard.receiveAttack([4, 5]);
+
+	expect(gameboard.isOver()).toBe(true);
+});
+
+test("if ships are placed and some sunk and others didn't the game shouldn't be over", () => {
+	const gameboard = new Gameboard();
+	const coordinates1 = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	const coordinates2 = [
+		[4, 6],
+		[4, 7],
+		[4, 8],
+	];
+	gameboard.placeShip(coordinates1);
+	gameboard.placeShip(coordinates2);
+	gameboard.receiveAttack([4, 3]);
+	gameboard.receiveAttack([4, 4]);
+	gameboard.receiveAttack([4, 5]);
+
+	expect(gameboard.isOver()).toBe(false);
+});
+
+test("a sequence of mixed hits and misses each register correctly", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	gameboard.placeShip(coordinates);
+
+	expect(gameboard.receiveAttack([0, 0])).toBe("w");
+	expect(gameboard.receiveAttack([4, 3])).toBe("x");
+	expect(gameboard.receiveAttack([1, 1])).toBe("w");
+	expect(gameboard.receiveAttack([4, 4])).toBe("x");
+});
