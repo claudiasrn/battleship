59eeee65e70bcef051711206289ac4c5132b2518
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-27 02:59:47 +0200

    feat: add Ship class

---
+++ src/Ship.js
@@ -0,0 +1,24 @@
+export class Ship {
+    #length;
+    #timesHit;
+    #sunk;
+
+    constructor(length) {
+        this.#length = length;
+        this.#timesHit = 0;
+        this.#sunk = false;
+    }
+
+    hit() {
+        this.#timesHit++;
+    }
+
+    isSunk() {
+        if (this.#length <= this.#timesHit) {
+            this.#sunk = true;
+            return this.#sunk;
+        }
+
+        return this.#sunk;
+    }
+}
