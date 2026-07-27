1a80e1cf644843e92199b5e6a08b728e996a668d
claudiasrn <perezserranoclaudia@gmail.com>
2026-07-27 04:45:18 +0200

    test(Player): add tests

---
+++ src/__tests__/Player.test.js
@@ -0,0 +1,22 @@
+import { Player } from "../Player.js";
+
+test("player is created with the given type", () => {
+	const player = new Player("human");
+	expect(player.type).toBe("human");
+});
+
+test("player is created with their own gameboard", () => {
+	const player = new Player("human");
+	expect(player.gameboard).toBeDefined();
+});
+
+test("player's gameboard can have ships placed on it", () => {
+	const player = new Player("human");
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+
+	expect(player.gameboard.placeShip(coordinates)).toBe(true);
+});
