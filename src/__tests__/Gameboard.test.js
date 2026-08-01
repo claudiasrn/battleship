ff66db76034d8374538f6d4a745c5816dbd7556c
claudiasrn <perezserranoclaudia@gmail.com>
2026-08-01 21:33:55 +0200

    test(gameboard): cover placeShip return value and removeShip

--- src/__tests__/Gameboard.test.js
+++ src/__tests__/Gameboard.test.js
@@ -190,3 +190,108 @@ test("a hit ship's cell still holds the Ship instance, not overwritten by the at
 	gameboard.receiveAttack([4, 4]);
 	expect(shipBoard[4][3].isSunk()).toBe(true);
 });
+
+test("placeShip returns the created Ship instance on success", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	const placedShip = gameboard.placeShip(coordinates, "falcon9");
+
+	expect(placedShip).toBeInstanceOf(Ship);
+	expect(placedShip.name).toBe("falcon9");
+});
+
+test("placeShip returns false when coordinates overlap an existing ship", () => {
+	const gameboard = new Gameboard();
+	gameboard.placeShip(
+		[
+			[4, 3],
+			[4, 4],
+			[4, 5],
+		],
+		"falcon9",
+	);
+
+	const secondPlacement = gameboard.placeShip(
+		[
+			[4, 4],
+			[4, 5],
+			[4, 6],
+		],
+		"voyager",
+	);
+
+	expect(secondPlacement).toBe(false);
+});
+
+test("removeShip clears the ship's cells back to empty", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	const placedShip = gameboard.placeShip(coordinates, "falcon9");
+	gameboard.removeShip(placedShip);
+
+	const shipBoard = gameboard.getShipBoard();
+	expect(shipBoard[4][3]).toBe("o");
+	expect(shipBoard[4][4]).toBe("o");
+	expect(shipBoard[4][5]).toBe("o");
+});
+
+test("removeShip lets a new ship be placed on the freed cells", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	const placedShip = gameboard.placeShip(coordinates, "falcon9");
+	gameboard.removeShip(placedShip);
+
+	const replacement = gameboard.placeShip(coordinates, "voyager");
+	expect(replacement).not.toBe(false);
+	expect(gameboard.getShipBoard()[4][3].name).toBe("voyager");
+});
+
+test("removeShip excludes the ship from isOver's sunk check", () => {
+	const gameboard = new Gameboard();
+	const coordinates = [
+		[4, 3],
+		[4, 4],
+		[4, 5],
+	];
+	const placedShip = gameboard.placeShip(coordinates, "falcon9");
+	gameboard.removeShip(placedShip);
+
+	expect(gameboard.isOver()).toBe(undefined);
+});
+
+test("removeShip doesn't affect other ships still on the board", () => {
+	const gameboard = new Gameboard();
+	const shipToRemove = gameboard.placeShip(
+		[
+			[4, 3],
+			[4, 4],
+			[4, 5],
+		],
+		"falcon9",
+	);
+	gameboard.placeShip(
+		[
+			[7, 0],
+			[7, 1],
+			[7, 2],
+		],
+		"voyager",
+	);
+
+	gameboard.removeShip(shipToRemove);
+
+	expect(gameboard.receiveAttack([7, 0])).toBe("x");
+	expect(gameboard.isOver()).toBe(false);
+});
